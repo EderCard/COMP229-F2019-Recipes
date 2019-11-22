1cfebc4cb24e042839114f538095c12cfa674b06
EderCard <edersoncard@gmail.com>
2019-11-22 10:42:30 -0500

    Add Review class

---
+++ COMP229-F2019-Recipes/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP229_F2019_Recipes.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string Comment { get; set; }
+        public int Rate { get; set; }
+        public int RecipeId { get; set; }
+
+    }
+}
