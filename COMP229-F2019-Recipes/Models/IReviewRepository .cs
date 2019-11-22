a8e65f3e379da4689719c7d3d86aec74c3b2874b
EderCard <edersoncard@gmail.com>
2019-11-22 10:53:21 -0500

    Add IReviewRepository  class

---
+++ COMP229-F2019-Recipes/Models/IReviewRepository .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP229_F2019_Recipes.Models
+{
+    public interface IReviewRepository
+    {
+        IQueryable<Review> Reviews { get; }
+        void saveReview(Review review);
+        Review deleteReview(int ReviewId);
+    }
+}
